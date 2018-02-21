80953f9dceb5117ed8eb4e58630aa5ad56c8627d
Cyan <dawid.grygier5@gmail.com>
2018-02-21 10:16:54 +0100

    small cleaning

--- accounts.py
+++ accounts.py
@@ -1,12 +1,13 @@
 import csv
 
 def saving_accounts_and_pass(dictionary):
-    w = csv.writer(open("accounts.csv", "w"))
-    for key, val in dictionary.items():
-        w.writerow([key, val])
+    with open('accounts.csv', 'w') as infile:
+        w = csv.writer(infile)
+        for key, val in dictionary.items():
+            w.writerow([key, val])
         
 def load_accounts_and_pass():
-    with open('accounts.csv', mode='r') as infile:
+    with open('accounts.csv', 'r') as infile:
         reader = csv.reader(infile)
         accounts = {rows[0]:rows[1] for rows in reader}
     return accounts
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import ui
 
 def main():
