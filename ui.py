2e4b17d87731eb2b1fd1d4d797b07c0de2158f67
Cyan <dawid.grygier5@gmail.com>
2018-02-21 12:46:49 +0100

    cosmetic changes

--- ui.py
+++ ui.py
@@ -3,6 +3,7 @@ import sys
 import pictures
 import data_manager
 
+
 def print_menu(menu_commands):
     for option in menu_commands:
         print('(' + str(menu_commands.index(option) + 1) + ')' + '  ----->  ' + option)
@@ -33,18 +34,18 @@ def profile(login):
     while option != '3':
         print_menu(menu_commands)
         option = input('Choose an option: ')
-        if option == "0":
-            data_manager.import_from_file(login)
-        elif option == "1":
+        if option == "1":
+            picture = data_manager.import_from_file(login)
+            print(picture)
+        elif option == "2":
             colors = pictures.get_random_proportion()
             picture = pictures.generate_picture(colors)
             choose_picture(login, picture)
-
-        
+ 
 
 def menu():
     option = ''
-    menu_commands = ['Create an account', 'Log in', 'Save your changes & Quit']
+    menu_commands = ['Create an account', 'Log in', 'Quit']
     while option != '0':
         print ('Main menu:')
         print_menu(menu_commands)
@@ -62,26 +63,21 @@ def menu():
         else:
             display.print_command_result('TREHE IS NO SUCH OPTION')
 
+
 def choose_picture(login, picture):
     options = ["Pretty", "Ugly", "Masterpiece - save!"]
     print_menu(options)
     decision = input("How do you like this picture?\n")
 
 
-    if decision == "0":
+    if decision == "1":
         pictures.change_picture(picture)
 
-    elif decision == "1":
+    elif decision == "2":
         percent_of_change = 0.6
         pictures.change_picture(picture, percent_of_change)
 
-    elif decision == "2":
+    elif decision == "3":
         data_manager.export_to_file(login, picture)
         "Your picture is saved in gallery"
-
-
-
-
-
-
 
