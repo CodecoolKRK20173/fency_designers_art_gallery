6d5505af5abc8f9b923a32245dafbcad46157c41
Szwajcii <d.szwajkos@gmail.com>
2018-02-21 12:05:48 +0100

    New function - display picture.

--- pictures.py
+++ pictures.py
@@ -40,22 +40,31 @@ def generate_picture(color_list):
     number_of_columns = int(input("Please enter width of picture: ")) * 2
     number_of_rows = int(input("Please enter heigh of picture: "))
 
-    characters_number = number_of_columns * number_of_rows 
+
+    colors = random.choice(color_list) + sign
+
     characters_list = []
 
-    for index in range(characters_number):
-        characters_list.append(random.choice(color_list) + sign)
+    element = [""] * number_of_columns
 
-    NORMAL = "\033[0m"
-
-    k = number_of_columns - 1
-    j = 0
     for i in range(number_of_rows):
-        print("".join(characters_list[j:k]) + NORMAL)
-        j = k
-        k += number_of_columns - 1
+        characters_list.append(element[:])
+
+    for line in characters_list:
+        for element in range(len(line)):
+            colors = random.choice(color_list) + sign
+            line[element] = colors
 
     return characters_list
+
+
+def display_picture(characters_list):
+    
+    NORMAL = "\033[0m"
+
+    for line in characters_list:
+        print("".join(line) + NORMAL)
+
 
 def change_picture(characters_list, percent_of_change = 0.2):
     x = len(characters_list)
@@ -70,7 +79,8 @@ def change_picture(characters_list, percent_of_change = 0.2):
 
 def main():
     color_list = get_random_proportion()
-    generate_picture(color_list)
+    characters_list = generate_picture(color_list)
+    display_picture(characters_list)
 
 
 if __name__ == "__main__":
